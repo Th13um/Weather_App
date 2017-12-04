7c7135d9f37414ef15cf87b3f8f32ff5dcdd8296
Th13um <dthieum@gmail.com>
2017-12-04 23:31:42 +0100

    add script for switch degree and add some css tweak

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
-
-
 	var long;
 	var lat;
-  var celsius;
-  var fahrenheit;
-
-
-		navigator.geolocation.getCurrentPosition(function(position){
-
-			long = position.coords.longitude;
-			lat = position.coords.latitude;
-
-
-var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&lang=fr'+'&units=metric&appid=d475e2ed504ab40f4de6c1b3cba9ebcc';
-
-$.getJSON(url, function(data){
-var weatherType = data.weather[0].description;
-var windSpeed = data.wind.speed;
-var icon = data.weather[0].icon;
-var city = data.name;
-var country = data.sys.country;
-var description = data.weather[0].description;
-var celsius = data.main.temp;
-fahrenheit = celsius + 9/5 +32;
-
-  $('.card').html( city + '<br> Temp: '+celsius+' °C'+ '<br> Wind Speed :'+windSpeed+'M/s');
-  $('.icon').html('<img src="http://openweathermap.org/img/w/' + icon + '.png" /> ' + '<br>'+weatherType);
-
-    console.log(city);
-		console.log(weatherType);
-		console.log(windSpeed);
-		console.log(icon);
+	// geolocation of the browser
+	navigator.geolocation.getCurrentPosition(function(position){
+		long = position.coords.longitude;
+		lat = position.coords.latitude;
+		//Get Location weather Info
+		var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&lang=fr'+'&units=metric&appid=d475e2ed504ab40f4de6c1b3cba9ebcc';
+		$.getJSON(url, function(data){
+			var weatherType = data.weather[0].description;
+			var windSpeed = data.wind.speed;
+			var icon = data.weather[0].icon;
+			var city = data.name;
+			var country = data.sys.country;
+			var description = data.weather[0].description;
+			var celsius = data.main.temp;
+			var fahrenheit = (celsius * 9/5 +32).toFixed(2);
+			var Temperature = celsius.toFixed(2);
+		//print the data on screen
+			  $('.city').html(city +", "+ country);
+				$('.wind').html('Wind Speed :'+ windSpeed +'M/s');
+				$('.temperature').html('Temperature: '+ Temperature +'°C');
+			  $('.icon').html('<img src="http://openweathermap.org/img/w/'+ icon +'.png" /> ' +'<br>'+ weatherType);
+		//function to change the degree units
+				$(".degree").on("click", function(){
+					if(Temperature === celsius){
+					$('.temperature').html('Temperature: '+fahrenheit+'°F');
+					Temperature = fahrenheit;
+				}else{
+					$('.temperature').html('Temperature: '+celsius+'°C');
+					Temperature = celsius;
+					}
+				})
       })
     })
 });
